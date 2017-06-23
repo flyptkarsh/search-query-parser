4ad808d33f5d1d5e832237cec35197720c5d750b
Patrick Karsh <ptkarsh@gmail.com>
2017-06-23 15:24:15 -0400

    Initial commit

---
+++ search_query_parser.rb
@@ -0,0 +1,41 @@
+class SearchQueryParser
+
+  def initialize(string)
+    @text = string.gsub(/'([^']*)'/,'').split("tag:").join('').strip
+    @tags = string.scan(/'([^']*)'/).flatten
+  end
+
+  def text
+     @text
+  end
+
+  def tags
+     @tags
+  end
+
+end
+
+describe SearchQueryParser do
+  describe '#text' do
+    it "finds the text" do
+      parser = described_class.new("tag:'monkey' tag:'monkey business' monkey")
+      expect(parser.text).to eql('monkey')
+    end
+    it "Finds the text whitout tags" do
+      parser = described_class.new('monkey banana fun-time')
+      expect(parser.text).to eql('monkey banana fun-time')
+    end
+  end
+
+
+  describe '#tags' do
+    it "finds the tags" do
+      parser = described_class.new("tag:'monkey' tag:'monkey business' monkey")
+      expect(parser.tags).to eql(['monkey', 'monkey business'])
+    end
+    it "finds the tags without text" do
+      parser = described_class.new("tag:'monkey' tag:'monkey business'")
+      expect(parser.tags).to eql(['monkey', 'monkey business'])
+    end
+  end
+end
